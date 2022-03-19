729231ff04d06276d7e5845822364aabcaf1a8c5
paustinch <101940619+paustinch@users.noreply.github.com>
2022-03-20 00:22:27 +0800

    Create _-*.lua
    note for todo

---
+++ share/scripts/_-*.lua
@@ -0,0 +1,41 @@
+--custom group tracks
+--get selected tracks
+--enter new name
+--left click
+--group
+--new group
+--insert name
+--unselect gain, muting, soloing, record enable, selection, active state, monitoring
+
+--select arm tracks
+--get selected tracks
+--if selected tracks has midi
+--then diconnect all input from all midi tracks
+--connect selected midi tracks to input
+--unarm all tracks
+--arm selected tracks
+
+--scala to mts
+--?
+
+--tracks to bus
+--get selected tracks
+--if selected tracks has midi tracks
+--then create midi bus
+--route bus output to track input
+--create audio bus
+--route track output to bus input and bus output to master
+
+--lfo gts range
+--change region to range?
+
+--export muiltiple regions
+--get selected regions
+--wav/mp3 extreme rate
+--right click
+--default export location/no name/export
+--repeat for multiple regions
+
+--unmute all region
+--find muted region
+--unmute
